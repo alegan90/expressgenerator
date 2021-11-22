a2a229f318c0079786d86b3bdac9cfe2fc1513c6
alegan90 <almelton90@icloud.com>
2021-11-22 11:18:20 -0800

    HTTPS

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ const mongoose = require('mongoose');
 
 var app = express();
 
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(`Redirecting to: https://${req.hostName}: ${app.get('secPort')}${req.url}`);
+    res.redirect(301, `https://${req.hostName}: ${app.get('secPort')}${req.url}`);
+  }
+});
+
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
